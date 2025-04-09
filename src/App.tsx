d85a2e18b16b6b594cdc83f0eb3726d79025bc1f
vefa <44091688+veefa@users.noreply.github.com>
2025-04-09 23:33:45 +0100

    Create and Clean up the App

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+
+  return (
+    <>
+
+      <div className="card">
+
+
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
