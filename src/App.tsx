4cea71607b28df1460e0ee50a1e377e6046f46b1
vefa <44091688+veefa@users.noreply.github.com>
2025-04-10 23:09:21 +0100

    Design the ChoreWheel

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ChoreWheel from './components/ChoreWheel';
 
-
 const App: React.FC = () => {
   return (
-    <div className="flex justify-center items-center bg-gray-100 min-h-screen">
-      <h1 className="font-bold text-gray-800 text-3xl">Chore Wheel App</h1>
-      <ChoreWheel/>
+    <div className="flex flex-col justify-center items-center bg-gray-100 p-4 min-h-screen">
+      <h1 className="mb-6 font-bold text-gray-800 text-3xl">Chore Wheel App</h1>
+      <ChoreWheel />
     </div>
   );
 };
